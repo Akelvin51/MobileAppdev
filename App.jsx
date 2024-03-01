65a49792b194a42daf4f66ccaab1e01a2bef6e6c
Kelvin <124754990+Akelvin51@users.noreply.github.com>
2024-03-01 15:52:56 -0700

    add lab3

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@
  *
  * @format
  */
-
+import { useState } from 'react';
 import React from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
@@ -12,11 +12,17 @@ import {
 } from 'react-native';
 
 function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
   return (
     <SafeAreaView>
-        <TodoList/> 
-       <TodoForm/>  
+      <TodoList tasks={tasks} />
+      <TodoForm/>
+
     </SafeAreaView>
+       
+
   );
 }
+
 export default App;
